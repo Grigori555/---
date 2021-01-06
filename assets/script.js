8fa2d5f7ea312c2a2f2a1c87dd5820b085b5f060
grisha <morozehs@yandex.ru>
2021-01-06 15:08:38 +0300

    0601 15:08

--- assets/script.js
+++ assets/script.js
@@ -9,10 +9,32 @@ const inputRound = document.getElementById("small_round");
 
 const inputCross =document.getElementById("small_cross");
 
+// практика в пррототипировании
+
+// reset
+const button = new Object({
+    name:"reset",
+    source:document.querySelector(".reset_game"),
+  
+})
+
+Object.prototype.removeDisable = function(){
+    button.source.removeAttribute("disabled")
+}
+
+
+Object.prototype.resetGame = function(element){
+    element.classList.add("hide");
+}
+
+button.removeDisable();
+
+
+// создай кнопку в объекте html
 
 
 
-
+//изменение атрибута инпутов радио
 inputRound.addEventListener("click",()=>{
    if((inputRound.hasAttribute("checked")===false) && inputCross.hasAttribute("checked")){
     inputRound.setAttribute("checked","checked");
@@ -32,20 +54,40 @@ inputCross.addEventListener("click",()=>{
 
 console.log(str)
 
-
+// построение фигур
  str.forEach((element)=>{
     element.addEventListener("click",()=>{
         if(cross.hasAttribute("checked")){
-            crossFig.classList.remove("hide");
-            roundFig.classList.add("hide");
+            element.children[0].classList.remove("hide"); // 0
+            // element.children[1].classList.add("hide"); //1
+            if(element.children[1]){
+            element.removeChild(element.children[1])
+        }
+            
     
         }
         if(round.hasAttribute("checked")){
-            roundFig.classList.remove("hide");
-            crossFig.classList.add("hide");
+            element.children[1].classList.remove("hide"); //1
+            // element.children[0].classList.add("hide"); //0
+            if(element.children[0]){
+            element.removeChild(element.children[0]);
+            }
         }
     })
     
  })
 
+
+//  сброс игры
+
+
+// button.source.onclick = ()=>{
+
+
+//     str.forEach((element)=>{
+//        element.children[0].classList.add("hide");
+      
+//     })
+   
+// }
 
